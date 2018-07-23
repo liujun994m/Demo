2c8f3187e39eba9bb8516265d3c73254ff79ab90
jun05.liu <jun05.liu@phicomm.com>
2018-07-23 17:43:35 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li>
+        <a
+          href="/api/untitled/index.html"
+        >
+          斐讯打赏
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Phicomm'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/RewardHome.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="RewardTopPhi">
+    <img src="./assets/phicomm.png">
+    <router-view/>
+  </div>
+  <div class="RewardTopWifi">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+  <div class="RewardMid">
+    <img src="./assets/invalid-name.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "RewardHome"
+    }
+</script>
+
+<style scoped>
+
+</style>
